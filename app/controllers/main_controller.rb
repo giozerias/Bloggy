c40d0842a69360c1416121da7aa8aab31863bbdf
giozerias <macbookpro@MacBooks-MacBook-Pro-2.local>
2013-11-20 20:49:36 -0500

    Bloggy Creation

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,19 @@
+class MainController < ApplicationController
+  def about_us
+  #Rails.logger.info "I am Gio"
+
+  #@founders = ["Richard Simms", "Betsy Houser III"]
+
+  @company_name = "Gio Corporation"
+  end
+
+  def privacy
+  end
+
+  def faq
+  end
+
+  def tos
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bloggy::Application.config.secret_key_base = '1dae83537de8d3548ce926a23a4b960b3fe9496c90c42af1fe5bbf1558b97fdc063e24c9ec5ad1f2f7646d1e4d34c4bcbf741a75acb71325489a52d6cd449e77'
